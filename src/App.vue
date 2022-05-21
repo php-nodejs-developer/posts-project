aa8109814a612b44cfaa8c3ed7929bef1eb740fd
aud203 <aud203@gmail.com>
2022-05-21 13:32:05 +0300

    Файлы лекции

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
    <!-- внешний вид компонента,
     в template должен быть хотя бы один тег-->
-  <nav></nav>
+  <posts-menu :menu-links="links"/>
+  <!-- <PostsMenu> -->
+
+  <router-view/>
+
+  <!-- <RouterView></RouterView> -->
+  <!-- <RouterView/> -->
+  <!-- <router-view/> -->
+
 </template>
 
 <script>
+// 1. компонент необходимо импортировать
+// import PostsMenu from "./components/PostsMenu"
+import PostsMenu from "@/components/PostsMenu"
+// @ - псевдоним (алиас) для директории src
+
 // все необходимы импорты, например импорт других компонентов,
 // которые будут использоваться в текущем
 
 export default { /* объект компонента */
-
+  // 2. регистрация импортированного компонента в текущем
+  // 3. использование в template
+  components: {PostsMenu  /* 'app-menu': PostsMenu */},
+  // ключи в components - названия, которые используются в html
+  // значения в components - объект компонента (из импорта)
+  data(){
+    return {
+      links: [
+        {title: "Главная", href: "/"},
+        {title: "Все записи", href: "/posts"}
+      ]
+    }
+  },
+  errorCaptured(err, compWithErr, errInfo) {
+    console.log(err, compWithErr, errInfo);
+    return false;
+  }
 }
 </script>
 
---
+++ src/components/PostsMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- PostsMenu.vue -->
+  <nav>
+    <router-link v-for="link in menuLinks"
+                 :to="link.href"
+                 :key="link.href">
+      {{ link.title }}
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    menuLinks: Array
+  }
+}
+</script>
+
